a5d65e29d8b31980112b18f9ef88eb4352148173
imi21123 <imi21123@naver.com>
2024-07-13 05:17:14 +0900

    Feat: App Router 설정

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,53 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import './App.css';
+import SignIn from './pages/Auth/SignIn';
+import SignUp from './pages/Auth/SignUp';
+import Auth from './pages/Main/Auth';
+import Home from './pages/Main/Home';
+import CreateNoticeRoom from './pages/Main/CreateNoticeRoom';
+import NoticeRoomCreated from './pages/Main/NoticeRoomCreated';
+import NoticeRoom from './pages/Main/NoticeRoom';
+import MemberList from './pages/MemberList/MemberList';
+import Main from './pages/Notice/Main';
+import Details from './pages/Notice/Details';
+import Join from './pages/Notice/Check/Join';
+import Quiz from './pages/Notice/Check/Quiz';
+import Results from './pages/Notice/Check/Results';
+import JoinType from './pages/Notice/Write/JoinType';
+import QuizType from './pages/Notice/Write/QuizType';
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    // <div className="App">
+    <Router>
+      <Routes>
+        {/* Auth - 로그인&회원가입 */}
+        <Route path="/sign-in" element={<SignIn />} />
+        <Route path="/sign-up" element={<SignUp />} />
+
+        {/* Main - 메인 */}
+        <Route path="/" element={<Auth />} />
+        <Route path="/home" element={<Home />} />
+        <Route path="/create-notice-room" element={<CreateNoticeRoom />} />
+        <Route path="/notice-room-created" element={<NoticeRoomCreated />} />
+        {/* <Route path="/notice-room" element={<NoticeRoom />} /> */}
+
+        {/* MemberList - 멤버 목록 */}
+        <Route path="/member-list" element={<MemberList />} />
+
+        {/* Notice - 공지 */}
+        <Route path="/notice" element={<Main />} />
+        <Route path="/notice/details" element={<Details />} />
+        <Route path="/notice/check-join" element={<Join />} />
+        <Route path="/notice/check-quiz" element={<Quiz />} />
+        <Route path="/notice/check-result" element={<Results />} />
+        <Route path="/notice/write-join" element={<JoinType />} />
+        <Route path="/notice/write-quiz" element={<QuizType />} />
+      </Routes>
+    </Router>
+    // </div>
+  );
 }
 
-export default App
+export default App;
