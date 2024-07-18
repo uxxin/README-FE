7e4b033ba5cb5c590807d346eb085cccb494472c
imi21123 <imi21123@naver.com>
2024-07-19 02:51:42 +0900

    Style: CustomInput 입력값 color 변경

--- src/components/CustomBtn.jsx
+++ src/components/CustomBtn.jsx
@@ -2,18 +2,6 @@ import React from 'react';
 import { useNavigate } from 'react-router-dom';
 import styled from 'styled-components';
 
-const StyledCustomBtn = styled.div`
-  display: flex;
-  padding: 1rem 9.6875rem;
-  justify-content: center;
-  align-items: center;
-  gap: 0.625rem;
-  border-radius: 0.5rem;
-  border: ${(props) => props.border};
-  background: ${(props) => props.background};
-  color: ${(props) => (props.background === '#FFFFFF' ? '#509BF7' : '#FFFFFF')};
-`;
-
 export const CustomBtn = ({ props }) => {
   const navigate = useNavigate();
   const handleClick = () => {
@@ -32,3 +20,15 @@ export const CustomBtn = ({ props }) => {
     </StyledCustomBtn>
   );
 };
+
+const StyledCustomBtn = styled.div`
+  display: flex;
+  padding: 1rem 9.6875rem;
+  justify-content: center;
+  align-items: center;
+  gap: 0.625rem;
+  border-radius: 0.5rem;
+  border: ${(props) => props.border};
+  background: ${(props) => props.background};
+  color: ${(props) => (props.background === '#FFFFFF' ? '#509BF7' : '#FFFFFF')};
+`;
