789fdeafcbce3c98d8342a1ff87bad208ecf195f
imi21123 <imi21123@naver.com>
2024-07-23 17:30:05 +0900

    Feat: 공지글 작성-입력 부분 퍼블리싱 완성

--- src/components/Notice/Write/Input.jsx
+++ src/components/Notice/Write/Input.jsx
@@ -1,35 +1,87 @@
-import React from 'react';
+import React, { useState, useRef, useEffect } from 'react';
 import styled from 'styled-components';
 
 export const Input = () => {
+  const [content, setContent] = useState('');
+  const contentInputRef = useRef(null);
+
+  useEffect(() => {
+    const textarea = contentInputRef.current;
+    textarea.style.height = 'auto';
+    textarea.style.height = `${textarea.scrollHeight}px`;
+  }, [content]);
+
+  const handleContentChange = (event) => {
+    setContent(event.target.value);
+  };
+
   return (
-    <div>
-      <Container>
-        <TitleInput
-          type="text"
-          placeholder="제목을 입력하세요"
-          maxLength={20}
-        />
-      </Container>
-    </div>
+    <Container>
+      <TitleInput type="text" placeholder="제목을 입력하세요" maxLength={20} />
+      <ContentInput
+        ref={contentInputRef}
+        value={content}
+        onChange={handleContentChange}
+        placeholder="내용을 입력하세요"
+      />
+    </Container>
   );
 };
 
 const Container = styled.div`
   display: flex;
+  width: 100%;
   min-height: 18.75rem;
   flex-direction: column;
   align-items: flex-start;
-  align-self: stretch;
-  border-radius: 0.5rem;
-  border: 0.5px solid var(--Primary-normal, #509bf7);
 `;
 
-const TitleInput = styled.div`
+const TitleInput = styled.input`
   display: flex;
   padding: 0.75rem 1.125rem;
   align-items: center;
   gap: 0.625rem;
   align-self: stretch;
   background: var(--Primary-Normal, #509bf7);
+  overflow: hidden;
+  color: var(--Basic-White, var(--Basic-White, #fff));
+  text-overflow: ellipsis;
+  font-family: Pretendard;
+  font-size: 1.125rem;
+  font-style: normal;
+  font-weight: 700;
+  line-height: 100%; /* 1.125rem */
+  letter-spacing: -0.0225rem;
+  outline: none;
+  border: 0.5px solid var(--Primary-normal, #509bf7);
+  border-top-left-radius: 0.5rem;
+  border-top-right-radius: 0.5rem;
+  ::placeholder {
+    color: var(--Text-emtpy, var(--Grayscale-Gray4, #bdbdbd));
+  }
+`;
+
+const ContentInput = styled.textarea`
+  display: flex;
+  padding: 20px;
+  flex-direction: column;
+  align-items: flex-start;
+  gap: 10px;
+  align-self: stretch;
+  background: var(--Basic-White, #fff);
+  outline: none;
+  border: 0.5px solid var(--Primary-normal, #509bf7);
+  color: var(--Text-default, var(--Grayscale-Gray7, #222));
+  font-family: Pretendard;
+  font-size: 1rem;
+  font-style: normal;
+  font-weight: 500;
+  line-height: 120%; /* 1.2rem */
+  letter-spacing: -0.02rem;
+  border-bottom-left-radius: 0.5rem;
+  border-bottom-right-radius: 0.5rem;
+  resize: none;
+  ::placeholder {
+    color: var(--Text-emtpy, var(--Grayscale-Gray4, #bdbdbd));
+  }
 `;
