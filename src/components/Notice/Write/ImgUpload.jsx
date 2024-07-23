85f42dc1190f21035cab9de92fa6b47d8c2bbdab
imi21123 <imi21123@naver.com>
2024-07-24 05:06:28 +0900

    Feat: 공지글 작성-사진 input 구현

---
+++ src/components/Notice/Write/ImgUpload.jsx
@@ -0,0 +1,131 @@
+import React, { useState } from 'react';
+import styled from 'styled-components';
+import camera from '../../../assets/images/img_upload.svg';
+import imgDelete from '../../../assets/images/img_delete.svg';
+
+export const ImgUpload = () => {
+  const [images, setImages] = useState([]);
+
+  const handleImageChange = (event) => {
+    const files = Array.from(event.target.files);
+    const newImages = files.map((file) => URL.createObjectURL(file));
+    setImages((prevImages) => [...prevImages, ...newImages].slice(0, 10));
+  };
+
+  const handleClick = () => {
+    document.getElementById('fileInput').click();
+  };
+
+  const handleDelete = (imageToDelete) => {
+    setImages(images.filter((image) => image !== imageToDelete));
+  };
+
+  return (
+    <UploadContainer>
+      <Label>사진 ({images.length}/10)</Label>
+      <ImagesContainer>
+        {images.length < 10 && (
+          <>
+            <HiddenInput
+              type="file"
+              accept="image/*"
+              multiple
+              onChange={handleImageChange}
+              id="fileInput"
+            />
+            <CameraIcon onClick={handleClick} isFull={images.length >= 10}>
+              <img src={camera} alt="Upload" />
+            </CameraIcon>
+          </>
+        )}
+        {images.slice(0, 9).map((image, index) => (
+          <ImagePreview key={index}>
+            <Image src={image} alt={`Preview ${index + 1}`} />
+            <ImageDelete
+              src={imgDelete}
+              alt="delete"
+              onClick={() => handleDelete(image)}
+            />
+          </ImagePreview>
+        ))}
+      </ImagesContainer>
+    </UploadContainer>
+  );
+};
+
+const UploadContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  gap: 0.625rem;
+`;
+
+const Label = styled.label`
+  color: var(--Text-default, var(--Grayscale-Gray7, #222));
+  font-family: Pretendard;
+  font-size: 1rem;
+  font-style: normal;
+  font-weight: 700;
+  line-height: 100%; /* 1rem */
+  letter-spacing: -0.02rem;
+`;
+
+const ImagesContainer = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 0.625rem;
+`;
+
+const HiddenInput = styled.input`
+  display: none;
+`;
+
+const CameraIcon = styled.div`
+  display: flex;
+  width: 6.25rem;
+  height: 6.25rem;
+  justify-content: center;
+  align-items: center;
+  border-radius: 0.5rem;
+  border: 0.33px solid
+    ${({ isFull }) =>
+      isFull
+        ? '#var(--GrayScale-gray4, #BDBDBD)'
+        : 'var(--Primary-light-active, #c9e0fd)'};
+  background: var(--Primary-light, #f4f9ff);
+  cursor: pointer;
+  img {
+    width: 1.5rem;
+    height: 1.5rem;
+  }
+`;
+
+const ImagePreview = styled.div`
+  display: flex;
+  width: 6.25rem;
+  height: 6.25rem;
+  justify-content: center;
+  align-items: center;
+  border-radius: 0.5rem;
+  border: 0.33px solid var(--Primary-light-active, #c9e0fd);
+  background: var(--Primary-light, #f4f9ff);
+  position: relative;
+`;
+
+const Image = styled.img`
+  width: 100%;
+  height: 100%;
+  object-fit: cover;
+  border-radius: 0.5rem;
+`;
+
+const ImageDelete = styled.img`
+  position: absolute;
+  top: 0.37rem;
+  right: 0.37rem;
+  width: 0.75rem;
+  height: 0.75rem;
+  z-index: 1;
+`;
+
+export default ImgUpload;
