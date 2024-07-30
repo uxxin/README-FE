9a5c22db3300be3ac419594bfce54192379ba5fc
imi21123 <imi21123@naver.com>
2024-07-31 05:32:28 +0900

    Feat: 공지글-질문 입력 퍼블리싱

---
+++ src/components/Notice/Write/QuestionInput.jsx
@@ -0,0 +1,52 @@
+import React, { useState } from 'react';
+import styled from 'styled-components';
+
+export const QuestionInput = ({ placeholder, charCount }) => {
+  const [inputValue, setInputValue] = useState('');
+
+  return (
+    <Container>
+      <Input
+        type="text"
+        placeholder={placeholder}
+        value={inputValue}
+        onChange={(e) => setInputValue(e.target.value)}
+        maxLength={charCount ? 20 : undefined}
+      />
+      {charCount ? <CharCount>({inputValue.length}/20)</CharCount> : <></>}
+    </Container>
+  );
+};
+
+const Container = styled.div`
+  display: flex;
+  padding: 0.5rem;
+  align-items: center;
+  flex: 1 0 0;
+  border-radius: 0.5rem;
+  border: 0.33px solid var(--Primary-Light-active, #c9e0fd);
+  background: var(--Primary-Light, #f4f9ff);
+`;
+
+const Input = styled.input`
+  border: none;
+  flex: 1;
+  outline: none;
+  background: none;
+  font-size: 1rem;
+  font-weight: 500;
+  line-height: 120%; /* 1.2rem */
+  letter-spacing: -0.02rem;
+  color: var(--Primary-normal, var(--Primary-Normal, #509bf7));
+  ::placeholder {
+    color: var(--Text-emtpy, var(--Grayscale-Gray4, #bdbdbd));
+  }
+`;
+
+const CharCount = styled.span`
+  color: var(--Text-caption, var(--Grayscale-Gray5, #888));
+  font-size: 0.75rem;
+  font-weight: 400;
+  line-height: 100%; /* 0.75rem */
+  letter-spacing: -0.015rem;
+`;
