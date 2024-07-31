71d03f7a52c1e309b6d8342d9fbfae4fa94c516f
Yeonri <lsy1307@inha.edu>
2024-07-31 19:09:36 +0900

    Feat: 공지방 메인 퍼블리싱 완료

--- src/components/CustomModal.jsx
+++ src/components/CustomModal.jsx
@@ -1,13 +1,10 @@
-import React from "react";
+import React, { useRef, useState, useEffect } from 'react';
 
-import styled from "styled-components";
+import styled from 'styled-components';
 
 const ModalOverlay = styled.div`
-  position: fixed;
-  top: 0;
-  left: 0;
+  position: absolute;
   right: 0;
-  bottom: 0;
   display: flex;
   justify-content: center;
   align-items: center;
@@ -16,24 +13,22 @@ const ModalOverlay = styled.div`
 
 const ModalContent = styled.div`
   background: transparent;
-  padding: 1.5rem;
-  width: 7.625rem; 
-  height: 4.5rem; 
   display: flex;
   flex-direction: column;
   justify-content: center;
   align-items: center;
 `;
 
-
 const ModalButton = styled.button`
   padding: 0.5rem 1rem;
   background-color: ${({ backgroundColor }) => backgroundColor || 'white'};
   color: ${({ color }) => color || 'black'};
-  border: 0.33px solid #BDBDBD;
+  border: 0.33px solid #bdbdbd;
   cursor: pointer;
-  width: 100%;
-  border-radius: ${({ isTop }) => (isTop ? '0.3125rem 0.3125rem 0 0' : '0 0 0.3125rem 0.3125rem')};
+  width: 7.625rem;
+  height: 2.25rem;
+  border-radius: ${({ isTop }) =>
+    isTop ? '0.3125rem 0.3125rem 0 0' : '0 0 0.3125rem 0.3125rem'};
   &:first-child {
     border-radius: 5px 5px 0 0;
   }
@@ -42,29 +37,48 @@ const ModalButton = styled.button`
   }
 `;
 
-
-
 const CustomModal = ({ isOpen, onClose, buttons }) => {
+  const modalRef = useRef(null);
+
+  useEffect(() => {
+    const handleClickOutside = (event) => {
+      const modalElement = modalRef.current;
+      const parentElement = modalElement ? modalElement.parentNode : null;
+      if (
+        !modalElement.contains(event.target) &&
+        !parentElement.contains(event.target)
+      ) {
+        onClose();
+      }
+    };
+
+    if (isOpen) {
+      document.addEventListener('mousedown', handleClickOutside);
+    }
+
+    return () => {
+      document.removeEventListener('mousedown', handleClickOutside);
+    };
+  }, [isOpen, onClose]);
+
   if (!isOpen) return null;
 
-  const handleModalContentClick = (e) => {
-    e.stopPropagation();
-  };
-
   return (
-    <ModalOverlay onClick={onClose}>
-      <ModalContent onClick={handleModalContentClick}>
-        {buttons && buttons.map((button, index) => (
-          <ModalButton
-            key={index} 
-            onClick={button.onClick} 
-            color={button.color}
-          >
-            {button.label}
-          </ModalButton>
-        ))}
+    <ModalOverlay ref={modalRef}>
+      <ModalContent>
+        {buttons &&
+          buttons.map((button, index) => (
+            <ModalButton
+              key={index}
+              onClick={button.onClick}
+              color={button.color}
+            >
+              {button.label}
+            </ModalButton>
+          ))}
       </ModalContent>
     </ModalOverlay>
   );
 };
+
 export default CustomModal;
