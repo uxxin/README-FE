0478f5935535c0e578ff2fc45d498d12ec6a477a
Haru0630 <hyeonjun@inha.edu>
2024-08-06 00:19:38 +0900

    Refactor: 코드 정리

--- src/api/RestApi.js
+++ src/api/RestApi.js
@@ -1,55 +1,68 @@
-import axios from "axios"
-import { UserApi } from "./UserApi"
+import axios from 'axios';
+import { UserApi } from './UserApi';
 
 export class RestApi {
+  static instance = new RestApi();
 
-  static instance = new RestApi()
-
-  client = axios.create({ baseURL: "http://read-me.kro.kr:8000" })
+  client = axios.create({ baseURL: 'http://read-me.kro.kr:8000' });
 
   /** @type number */
-  authorizationHeaderInterceptor
+  authorizationHeaderInterceptor;
 
-  user = new UserApi(this)
+  user = new UserApi(this);
 
   /** @param {string} token */
   applyJwt(token) {
-    localStorage.setItem("token", token)
+    localStorage.setItem('token', token);
     if (this.authorizationHeaderInterceptor)
-      this.client.interceptors.request.eject(this.authorizationHeaderInterceptor)
+      this.client.interceptors.request.eject(
+        this.authorizationHeaderInterceptor,
+      );
 
     this.authorizationHeaderInterceptor = this.client.interceptors.request.use(
-      config => {
-        config.headers = { ...config.headers, "Authorization": `Bearer ${token}` }
-        return config
-      }
-    )
+      (config) => {
+        config.headers = {
+          ...config.headers,
+          Authorization: `Bearer ${token}`,
+        };
+        return config;
+      },
+    );
   }
 
   clearJwt() {
-    localStorage.removeItem("token")
+    localStorage.removeItem('token');
     if (this.authorizationHeaderInterceptor)
-      this.client.interceptors.request.eject(this.authorizationHeaderInterceptor)
+      this.client.interceptors.request.eject(
+        this.authorizationHeaderInterceptor,
+      );
   }
 
+  /**
+   *  @returns {Promise<{ userId: number, name: string, nickname: string, email: string, profileImage: string } | null>}
+   */
   async autoLogin() {
-    const savedToken = localStorage.getItem("token")
-    if (!savedToken) return null
+    const savedToken = localStorage.getItem('token');
+    if (!savedToken) return null;
 
-    this.applyJwt(savedToken)
-    return await this.user.me().then(it => it.data)
+    this.applyJwt(savedToken);
+    return await this.user.me().then((it) => it.data.result);
   }
 
+  /**
+   *  @returns {Promise<{ userId: number, name: string, nickname: string, email: string, profileImage: string }>}
+   */
   async login(email, password) {
-    const { result: { accessToken } } = await this.user.login(email, password).then(it => it.data)
+    const {
+      result: { accessToken },
+    } = await this.user.login(email, password).then((it) => it.data);
 
-    this.applyJwt(accessToken)
-    const response = await this.user.me()
-    return response.data
+    this.applyJwt(accessToken);
+    const response = await this.user.me();
+    return response.data.result;
   }
 
   async logout() {
-    this.clearJwt()
+    this.clearJwt();
   }
-
 }
--- src/api/UserApi.js
+++ src/api/UserApi.js
@@ -1,22 +1,25 @@
 import { RestApi } from './RestApi.js';
-import axios from "axios"
+import axios from 'axios';
 
 export class UserApi {
-
   /** @type RestApi */
-  parent
+  parent;
+
+  axios = axios;
 
   /**
    *  @param {RestApi} parent
    */
-  constructor(parent) { this.parent = parent }
+  constructor(parent) {
+    this.parent = parent;
+  }
 
   /**
    *  @param {string} email
    *  @returns {Promise<axios.AxiosResponse<{ isSuccess: boolean, code: number, message: string, result: string }>>}
    */
   async createCode(email) {
-    return await this.parent.client.post("/user/create-code", { email })
+    return await this.parent.client.post('/user/create-code', { email });
   }
 
   /**
@@ -25,7 +28,7 @@ export class UserApi {
    *  @returns {Promise<axios.AxiosResponse<{ isSuccess: boolean, code: number, message: string, result: { verified: boolean } }>>}
    */
   async confirmCode(email, code) {
-    return await this.parent.client.post("/user/confirm-code", { email, code })
+    return await this.parent.client.post('/user/confirm-code', { email, code });
   }
 
   /**
@@ -36,12 +39,17 @@ export class UserApi {
    *  @returns {Promise<axios.AxiosResponse<{ isSuccess: boolean, code: number, message: string, result: { userId: number, accessToken: string } }>>}
    */
   async signup(name, nickname, email, password) {
-    return await this.parent.client.post("/user/signup", { name, nickname, email, password })
+    return await this.parent.client.post('/user/signup', {
+      name,
+      nickname,
+      email,
+      password,
+    });
   }
 
   /** @returns {Promise<axios.AxiosResponse<{ isSuccess: boolean, code: number, message: string, result: { userId: number, name: string, nickname: string, email: string, profileImage: string } }>>} */
   async me() {
-    return await this.parent.client.get("/user")
+    return await this.parent.client.get('/user');
   }
 
   /**
@@ -50,7 +58,6 @@ export class UserApi {
    *  @returns {Promise<axios.AxiosResponse<{ isSuccess: boolean, code: number, message: string, result: { userId: number, accessToken: string } }>>}
    */
   async login(email, password) {
-    return await this.parent.client.post("/user/login", { email, password })
+    return await this.parent.client.post('/user/login', { email, password });
   }
-
 }
