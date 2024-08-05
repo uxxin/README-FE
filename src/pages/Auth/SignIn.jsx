2e5015617a71e0134c91e828b8b3d148aa2a3b92
Haru0630 <hyeonjun@inha.edu>
2024-08-06 01:00:35 +0900

    Feat: 로그인 페이지 변경

--- src/pages/Auth/SignIn.jsx
+++ src/pages/Auth/SignIn.jsx
@@ -1,23 +1,91 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { AuthLogo } from '../../components/Auth/AuthLogo';
-import { AuthBtn } from '../../components/Auth/AuthBtn';
 import styled from 'styled-components';
-import { LoginInput } from '../../components/Auth/LoginInput';
+import CustomInput from '../../components/CustomInput.jsx';
+import { useUserContext } from '../../contexts/UserContext.jsx';
+import { useNavigate } from 'react-router-dom';
 
-const StyledAuth = styled.div`
+const SignIn = () => {
+  const navigate = useNavigate();
+
+  const user = useUserContext();
+
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  return (
+    <Root>
+      <AuthLogo />
+      <InputContainer>
+        <CustomInput
+          placeholder="이메일"
+          value={email}
+          onChange={(e) => setEmail(e.currentTarget.value)}
+          charCount={true}
+        />
+        <CustomInput
+          placeholder="비밀번호"
+          value={password}
+          onChange={(e) => setPassword(e.currentTarget.value)}
+          charCount={true}
+        />
+      </InputContainer>
+      <SignInButton
+        onClick={async () => {
+          await user.login(email, password);
+          navigate('/home');
+        }}
+      >
+        로그인
+      </SignInButton>
+      <NotAuth>아직 회원이 아니신가요?</NotAuth>
+      <SignupButton onClick={() => navigate('/sign-up')}>회원가입</SignupButton>
+    </Root>
+  );
+};
+
+const Root = styled.div`
   display: flex;
   flex-direction: column;
+  align-items: stretch;
   padding: 1.87rem 1rem;
 `;
 
-const SignIn = () => {
-  return (
-    <StyledAuth>
-      <AuthLogo />
-      <LoginInput />
-      <AuthBtn />
-    </StyledAuth>
-  );
-};
+const InputContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: stretch;
+  gap: 0.75rem;
+  position: relative;
+`;
+
+const CommonButton = styled.button`
+  padding: 1rem 0;
+  border-radius: 0.5rem;
+  border: none;
+  font-size: 1rem;
+  font-style: normal;
+  font-weight: 500;
+  width: 100%;
+`;
+
+const SignInButton = styled(CommonButton)`
+  margin-bottom: 0.625rem;
+  background-color: #509bf7;
+  color: #ffffff;
+`;
+const SignupButton = styled(CommonButton)`
+  background-color: #ffffff;
+  color: #509bf7;
+  border: 0.5px solid #509bf7;
+`;
+
+const NotAuth = styled.div`
+  align-self: stretch;
+  font-size: 0.875rem;
+  font-weight: 400;
+  line-height: 100%;
+  letter-spacing: -0.0175rem;
+`;
 
 export default SignIn;
