1871cf3cb6f1bf805935215adb6083269c0fb689
uxxin <dbwlsdid1114@naver.com>
2024-08-08 16:37:14 +0900

    Feat: 고정된 공지글 api 연동 완료

--- src/components/Main/FixedNotice.jsx
+++ src/components/Main/FixedNotice.jsx
@@ -6,40 +6,59 @@ import deleteIcon from '../../assets/images/deleteicon.svg';
 
 const FixedNotice = ({ onDelete }) => {
   const [notice, setNotice] = useState({
-    postId: 1,
+    roomId: 0,
+    postId: 0,
     title: '공지글 제목',
     startDate: '시작일',
     endDate: '마감일',
   });
 
   useEffect(() => {
-    axios
-      .get('/mock/FixedData.json')
-      .then((response) => {
-        setNotice(response.data);
-      })
-      .catch((error) => {
+    const getFixedNotice = async () => {
+      try {
+        const options = {
+          method: 'GET',
+          headers: {
+            Authorization: `Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOjU5LCJwcm92aWRlciI6IlJFQURNRSIsImlhdCI6MTcyMzEwMDE3NSwiZXhwIjoxNzIzMTEwOTc1fQ.c_hk6yPRxJYYrvDJeM72kpAJavFKjSUq1hhdJ3wrmIo`,
+          },
+        };
+        const response = await axios(
+          'https://read-me.kro.kr/user/fixed',
+          options,
+        );
+        if (response === null) {
+          return;
+        } else {
+          setNotice(response.data.result);
+        }
+      } catch (error) {
         console.error('공지 데이터를 가져오는 중 오류 발생:', error);
-      });
+      }
+    };
+    getFixedNotice();
   }, []);
 
   return (
-    <NoticeContainer>
-      <PinButton>
-        <img src={pinIcon} alt="Pin Icon" />
-      </PinButton>
-      <NoticeContent>
-        <NoticeTitle>{notice.title}</NoticeTitle>
-        <NoticeDate>
-          <Date>{notice.startDate}</Date>
-          <DateSeparator>-</DateSeparator>
-          <Date>{notice.endDate}</Date>
-        </NoticeDate>
-      </NoticeContent>
-      <DeleteButton onClick={onDelete}>
-        <img src={deleteIcon} alt="Delete Icon" />
-      </DeleteButton>
-    </NoticeContainer>
+    <>
+      {notice && (
+        <NoticeContainer>
+          <PinButton>
+            <img src={pinIcon} alt="Pin Icon" />
+          </PinButton>
+          <NoticeContent>
+            <NoticeTitle>{notice.title}</NoticeTitle>
+            <NoticeDate>
+              <Date>{notice.startDate}</Date>
+              <DateSeparator>-</DateSeparator>
+              <Date>{notice.endDate}</Date>
+            </NoticeDate>
+          </NoticeContent>
+          <DeleteButton onClick={onDelete}>
+            <img src={deleteIcon} alt="Delete Icon" />
+          </DeleteButton>
+        </NoticeContainer>
+      )}
+    </>
   );
 };
 
