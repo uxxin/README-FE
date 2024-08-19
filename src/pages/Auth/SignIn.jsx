6920dbf4cb1d9f05b9036b0e91a5b8d81b72e78f
gs0428 <lider28@naver.com>
2024-08-19 20:31:07 +0900

    fix: 모바일 화면에서 input 길이 표시하는 부분 넘치는 오류 해결

--- src/pages/Auth/SignIn.jsx
+++ src/pages/Auth/SignIn.jsx
@@ -1,17 +1,16 @@
 import React, { useState } from 'react';
 import styled from 'styled-components';
-import CustomInput from '../../components/CustomInput.jsx';
 import { useNavigate } from 'react-router-dom';
 
 import { login } from '../../api/Auth/user.js';
 import logo from '../../assets/svgs/logoex.svg';
 import KakaoLoginButton from '../../components/common/kakao-login/index.jsx';
+import Input from '../../components/common/input/index.jsx';
 
 const SignIn = () => {
   const navigate = useNavigate();
 
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
+  const [user, setUser] = useState({ email: '', password: '' });
 
   const apiKey = import.meta.env.VITE_KAKAO_REST_API_KEY;
   const redirectUri = import.meta.env.VITE_KAKAO_REDIRECT_URI;
@@ -36,22 +35,29 @@ const SignIn = () => {
     window.location.href = kakaoLoginPage;
   };
 
+  const handleChange = (e) => {
+    const { value, id } = e.target;
+    setUser({ ...user, [id]: value });
+  };
+
   return (
     <Root>
       <StyledLogo src={logo} alt="logo" />
       <InputContainer onSubmit={handleLogin}>
-        <CustomInput
-          type="email"
+        <Input
+          id="email"
+          type="text"
           placeholder="이메일"
-          value={email}
-          onChange={(e) => setEmail(e.currentTarget.value)}
+          value={user.email}
+          onChange={handleChange}
         />
-        <CustomInput
+        <Input
+          id="password"
           type="password"
           placeholder="비밀번호"
-          value={password}
-          onChange={(e) => setPassword(e.currentTarget.value)}
-          charCount={true}
+          value={user.password}
+          maxLength={20}
+          onChange={handleChange}
         />
         <ButtonWrapper>
           <SignInButton onClick={handleLogin}>로그인</SignInButton>
